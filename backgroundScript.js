39bdccd24a29153450e4a3c4900df831a21c6386
GerA <lpgera@gmail.com>
2024-10-13 22:18:05 +0200

    initial commit

---
+++ backgroundScript.js
@@ -0,0 +1,10 @@
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+      const url = new URL(details.url)
+      if (!url.searchParams.get('authuser') && details.type === "main_frame") {
+        url.searchParams.set('authuser', '1')
+        return { redirectUrl: url.href }
+      }
+    },
+    { urls: ["https://console.cloud.google.com/*"] },
+    ["blocking"]
+  )
